07cbd0ff33bb53a6c23e2d2b3854fed0c20e79e8
kotqtsu <131863139+TatsuyaKoyanagi@users.noreply.github.com>
2023-08-10 18:40:54 +0900

    Delete loading.js

--- momo-2022.4.1_windows-10.1809_x86_64/html/loading.js
+++
@@ -1,8 +0,0 @@
-function include_test(){
-    $.ajax({
-        url: 'test.html', // リクエストを送信するURLを指定
-        async: false, // 非同期リクエストを無効にする
-    }).done(function(test_html){ // 通信が成功したら
-        document.write(test_html); // herder.htmlを表示する
-    });
-}
