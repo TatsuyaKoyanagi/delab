e0a928df0163bc54d07920038ced367fcb0fbbf5
{tatsuya <{k.tatsuya150205@icloud.com}>
2023-10-05 19:45:28 +0900

    add some paturn

--- platformio_test/src/main.cpp
+++ platformio_test/src/main.cpp
@@ -3,15 +3,38 @@
 */
 #include <Arduino.h>
 
+void Ltika();
+void serial_test();
+int i=0;
+int root=2;
+
 void setup() {
     Serial.begin(9600);
     pinMode( 13, OUTPUT );
 }
+
 void loop() {
-    digitalWrite( 13, HIGH );
-    Serial.println("HIGH");
-    delay(100);
-    digitalWrite( 13, LOW );
-    Serial.println("LOW");
-    delay(200);
+  if(root==1){
+    Ltika();
+  }else if(root ==2){
+    serial_test();
+  }
+}
+
+void Ltika(){
+  digitalWrite( 13, HIGH );
+  Serial.println("HIGH");
+  delay(100);
+  digitalWrite( 13, LOW );
+  Serial.println("LOW");
+  delay(200);
+}
+
+void serial_test(){
+  Serial.println(i);
+  delay(100);
+  i++;
+  if(i==100){
+    i=0;
+  }
 }
