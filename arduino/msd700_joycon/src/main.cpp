1f8d672136ff7eb815961fec8e263f2110374be7
{tatsuya <{k.tatsuya150205@icloud.com}>
2023-10-08 17:36:55 +0900

    add 1st line

--- arduino/msd700_joycon/src/main.cpp
+++ arduino/msd700_joycon/src/main.cpp
@@ -1,18 +1,97 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int INPUT_PINS[] = { A0, A1, A2, A3 };
+const int NUM_PINS = sizeof(INPUT_PINS) / sizeof(INPUT_PINS[0]);
+const int MID = 512;
+int INPUT_RES[NUM_PINS];
+int OFFSET_RES[NUM_PINS];
+
+volatile int flag = 0;
+
+void set_offset();
+void serialEvent();
+void read_resister();
+void print_resister();
+void send_resister();
+void pause();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  for (int i = 0; i < NUM_PINS; i++) {
+    pinMode(INPUT_PINS[i], INPUT);
+  }
+  Serial.begin(9600);
+  set_offset();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(flag==1){
+    pause();
+  }else{
+  read_resister();
+  print_resister();
+  //send_resister();
+  Serial.print(0);
+  Serial.print(",");
+  Serial.print(1000);
+  Serial.print(",");
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void set_offset() {
+  for (int i = 0; i < NUM_PINS; i++) {
+    INPUT_RES[i] = analogRead(INPUT_PINS[i]);
+  }
+  for (int i = 0; i < NUM_PINS; i++) {
+    OFFSET_RES[i] = MID - INPUT_RES[i];
+  }
+}
+
+void serialEvent() {  // シリアルイベント関数
+  while (Serial.available()) {
+    char receivedChar = Serial.read();
+    //offsetのリセット
+    if (receivedChar == 'r' || receivedChar == 'R') {
+      set_offset();
+    }
+    //一時停止
+    if (receivedChar == 'c' || receivedChar == 'C') {
+      flag = 1;
+    }
+    //一時停止解除
+    if (receivedChar == 's' || receivedChar == 'S') {
+      flag = 0;
+    }
+  }
+}
+
+void read_resister() {
+  for (int i = 0; i < NUM_PINS; i++) {
+    INPUT_RES[i] = analogRead(INPUT_PINS[i]) + OFFSET_RES[i];
+    //INPUT_RES[i] = map(INPUT_RES[i], OFFSET_RES[i], 1023 + OFFSET_RES[i], 0, 1023);
+  }
+}
+
+void print_resister() {
+  for (int i = 0; i < NUM_PINS; i++) {
+    Serial.print(INPUT_RES[i]);
+    if (i != NUM_PINS - 1) {
+      Serial.print(",");
+    }
+  }
+  Serial.println();
+}
+
+void send_resister() {
+  String resStr = "";
+  for (int i = 0; i < NUM_PINS; i++) {
+    resStr += String(INPUT_RES[i]);
+    if (i != NUM_PINS - 1) {
+      resStr += ",";
+    }
+  }
+  Serial.println(resStr);
+}
+
+void pause(){
+  delay(1);
 }
